08c48898fc2454bc3903eb3adb9dc0654db8d072
Shawn George <shawngeorge530@gmail.com>
2024-11-11 22:43:08 +1300

    Adding all the project files for this project

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import Main from './Components/Main'
+
+export default function App() {
+  return(
+    <div className = "Container">
+      <Navbar />
+      <Main />
+    </div>
+  )
+}
+
+
---
+++ vite-project/src/Components/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Main() {
+    return (
+        <main>
+            <h1 className="Topic"> Fun Facts about React</h1>
+            <ul className="Bullets">
+                <li>was first released in 2013</li>
+                <li>was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+    )
+}
